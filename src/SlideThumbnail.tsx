f1bbfc04ce72fc5c7b9350a644ebd30fa18679f2
Merrywindl <whasup42@hotmail.com>
2025-02-13 21:16:29 -0600

    size testing

--- src/SlideThumbnail.tsx
+++ src/SlideThumbnail.tsx
@@ -15,7 +15,7 @@ export default function SlideThumbnail({slide, onSelected,deleteSlide,  isSelect
         
         <div  className="vstack g-3 p-3 d-flex align-items-center ">
             <span className="fw-bolder text-center align-self-center">{slide.text}</span>
-            <img src={slide.image} width={200} height={200} onClick={() => onSelected(slide.id)} 
+            <img src={slide.image} width={'200rem'} height={'200rem'} onClick={() => onSelected(slide.id)} 
             className={`border border-primary border-5  ${isSelected ? 'rounded-5' : 'border-0'} ${isSelected ? '' : 'bg-transparent'}`} />
             <button className="bg-danger rounded-5 text-success" style={{ width: '200px' }}  onClick={() => deleteSlide(slide.id)}>Delete</button>
         </div>

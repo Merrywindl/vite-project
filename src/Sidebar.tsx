31fad764dcdee2bd62e9c86a0a86d04461168c09
Merrywindl <whasup42@hotmail.com>
2025-02-14 00:30:58 -0600

    size testing

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -15,40 +15,37 @@ export default function Sidebar({ addBlankSlide }: SidebarProps) {
     <div style={{ display: "flex" }}>
       {isExpanded ? (
         <div
-          className="border-black border-end border-5 bg-transparent p-5 d-flex flex-column vh-100"
-          style={{ width: "20vw", maxWidth: "20vw" }} // Set width to 20vw
+          className="border-black border-end border-5 bg-transparent p-2 d-flex flex-column"
+          style={{ width: "20vw", maxWidth: "20vw" }}
         >
-          <div>
-            <form>
-              <input
-                className="w-100 border border-black border-5 p-2 bg-light bg-opacity-25 font-weight-bold mb-2" // Added mb-2 for spacing
-                type="text"
-                id="inputID"
-                placeholder="Coming Soon"
-              />
-              <input
-                className="w-100 border border-black border-5 p-2 bg-light bg-opacity-25 font-weight-bold mb-2"
-                type="text"
-                id="inputID"
-                placeholder="Coming Soon"
-              />
-              <input
-                className="w-100 border border-black border-5 p-2 bg-light bg-opacity-25 font-weight-bold mb-2"
-                type="text"
-                id="inputID"
-                placeholder="Coming Soon"
-              />
+          <form style={{ width: "100%" }}>
+            <input
+              className="w-75 m-2 border border-black border-5 p-2 bg-light bg-opacity-25 font-weight-bold"
+              type="text"
+              id="inputID"
+              placeholder="Coming Soon"
+            />
+            <input
+              className="w-75 m-2 border border-black border-5 p-2 bg-light bg-opacity-25 font-weight-bold"
+              type="text"
+              id="inputID"
+              placeholder="Coming Soon"
+            />
+            <input
+              className="w-75 m-2 border border-black border-5 p-2 bg-light bg-opacity-25 font-weight-bold"
+              type="text"
+              id="inputID"
+              placeholder="Coming Soon"
+            />
 
-              <button
-                className="btn btn-primary border border-black border-5 w-100"
-                onClick={addBlankSlide}
-              >
-                Enter
-              </button>
-            </form>
-          </div>
-          <h1 className="mt-3">Please excuse our dust</h1>{" "}
-          {/* Added mt-3 for margin top */}
+            <button
+              className="btn btn-primary mt-2 border border-black border-5 w-75"
+              onClick={addBlankSlide}
+            >
+              Enter
+            </button>
+          </form>
+          <h1 className="mt-3">Please excuse our dust</h1>
         </div>
       ) : null}
       <button

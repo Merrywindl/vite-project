2e2e6863e03fa8170c8d08422c9a22135233d63f
Merrywindl <whasup42@hotmail.com>
2025-02-13 21:25:36 -0600

    ..

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -17,7 +17,7 @@ export default function Sidebar({addBlankSlide} : SidebarProps) {
     return (
         <>
             {isExpanded ? (
-                <div className=" border-black border-end border-5 bg-transparent p-5 d-flex flex-column vh-100rem  max w-20rem">
+                <div className=" border-black border-end border-5 bg-transparent p-5 d-flex flex-column vh-100rem  max w-10rem">
                     <div>
                         <form>
                             <input  className="max w-100 border border-black border border-5 p-2 bg-light bg-opacity-25 font-weight-bold" type="text" id="inputID" placeholder="Coming Soon"/>                               
